e2af52ab93f4b6f67a8dedb0352cd76611dfc658
Tsivinsky.Petr <creatsiv@gmail.com>
2019-06-05 13:26:27 +0300

    Transpile index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-import SequentialEntrance from "./SequentialEntrance-transplied";
+"use strict";
 
-const VueSequentialEntrance = {
-  install: function(Vue, options) {
-    Vue.component("sequential-entrance", SequentialEntrance);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _SequentialEntranceTransplied = _interopRequireDefault(require("./SequentialEntrance-transplied"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var VueSequentialEntrance = {
+  install: function install(Vue, options) {
+    Vue.component("sequential-entrance", _SequentialEntranceTransplied["default"]);
   }
 };
-
-export default VueSequentialEntrance;
+var _default = VueSequentialEntrance;
+exports["default"] = _default;
